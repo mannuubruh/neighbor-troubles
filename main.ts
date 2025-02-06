f87f6e7ec2b6ea479e2b650dcaba8243224afc79
mannuubruh <27mmanning@aos92.org>
2025-02-05 21:39:34 -0500

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,54 @@
-
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile11`, function (sprite, location) {
+    game.showLongText("Claire: Hey Mom?", DialogLayout.Bottom)
+    tiles.setCurrentTilemap(tilemap`level1`)
+    game.showLongText("Mom: Yes hun?", DialogLayout.Bottom)
+    game.showLongText("Claire: Why is the door just a Black rectangle?", DialogLayout.Bottom)
+    game.showLongText("Mom: I actually don't know.", DialogLayout.Bottom)
+    game.showLongText("Claire: Do you think it is cuz the developer of this game was to lazy to draw an original door sprite and in turn made this poorly written joke as an excuse to not draw one?", DialogLayout.Bottom)
+    game.showLongText("Mom: ...", DialogLayout.Bottom)
+    game.showLongText("Mom: sure. what ever makes you happy.", DialogLayout.Bottom)
+    game.showLongText("Mom: Now go eat please.", DialogLayout.Bottom)
+    game.showLongText("Claire: OK!", DialogLayout.Bottom)
+})
+game.splash("Hello?")
+game.splash("Can you hear me?")
+game.splash("...")
+game.splash("Good.")
+game.splash("very Good.")
+game.splash("Listen closely.")
+game.splash("when you wake up.")
+game.splash("weird thing will happen.")
+game.splash("I cant specify what.")
+game.splash("but it will be weird.")
+game.splash("your")
+game.splash("name")
+game.splash("Is")
+game.showLongText("CLAIRE!!!", DialogLayout.Bottom)
+let mySprite = sprites.create(img`
+    . . . . . f f 4 4 f f . . . . . 
+    . . . . f 5 4 5 5 4 5 f . . . . 
+    . . . f e 4 5 5 5 5 4 e f . . . 
+    . . f b 3 e 4 4 4 4 e 3 b f . . 
+    . . f 3 3 3 3 3 3 3 3 3 3 f . . 
+    . f 3 3 e b 3 e e 3 b e 3 3 f . 
+    . f 3 3 f f e e e e f f 3 3 f . 
+    . f b b f b f e e f b f b b f . 
+    . f b b e 1 f 4 4 f 1 e b b f . 
+    f f b b f 4 4 4 4 4 4 f b b f f 
+    f b b f f f e e e e f f f b b f 
+    . f e e f b d d d d b f e e f . 
+    . . e 4 c d d d d d d c 4 e . . 
+    . . e f b d b d b d b b f e . . 
+    . . . f f 1 d 1 d 1 d f f . . . 
+    . . . . . f f b b f f . . . . . 
+    `, SpriteKind.Player)
+tiles.setCurrentTilemap(tilemap`level2`)
+tiles.placeOnRandomTile(mySprite, assets.tile`myTile`)
+scene.cameraFollowSprite(mySprite)
+controller.moveSprite(mySprite)
+game.showLongText("Mom: Claire, wake up!", DialogLayout.Bottom)
+game.showLongText("Mom: Its a beautiful day outside and your wasting it all laying in bed.", DialogLayout.Bottom)
+game.showLongText("Mom: Get up lazy bones!", DialogLayout.Bottom)
+game.showLongText("Clair: yes ma'am.", DialogLayout.Bottom)
+game.showLongText("Mom: I made you breakfast. it is waiting in the kitchen.", DialogLayout.Bottom)
+game.showLongText("Claire: Thank you mom!", DialogLayout.Bottom)
---
+++ tilemap.g.ts
@@ -0,0 +1,112 @@
+// Auto-generated code. Do not edit.
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency16 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile1 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile2 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile4 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile5 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile6 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile7 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile8 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile9 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile10 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile11 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile12 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile13 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile14 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile3 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile15 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile17 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile16 = image.ofBuffer(hex``);
+
+    helpers._registerFactory("tilemap", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "level1":
+            case "level1":return tiles.createTilemap(hex`0d000900000000000000000000000000000005050503050505030505050000050c0504050a05040505050000050c0505050b05050505010000111414141414101414140200001215150617070d15151518000012150e0817090f15151518000013161616161616161616190000000000000000000000000000`, img`
+2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 . . . . . 2 . . . . . 2 
+2 . . . . . . . . . . . 2 
+2 . . . . . . 2 . . . . 2 
+2 . . . . . . . . . . . 2 
+2 . . . . . . . . . . . 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 
+`, [myTiles.transparency16,myTiles.tile1,myTiles.tile2,myTiles.tile4,myTiles.tile5,myTiles.tile6,myTiles.tile7,myTiles.tile8,myTiles.tile9,myTiles.tile10,myTiles.tile11,myTiles.tile12,myTiles.tile13,myTiles.tile14,myTiles.tile3,myTiles.tile15,myTiles.tile16,sprites.dungeon.darkGroundNorthWest0,sprites.dungeon.darkGroundWest,sprites.dungeon.darkGroundSouthWest0,sprites.dungeon.darkGroundNorth,sprites.dungeon.darkGroundCenter,sprites.dungeon.darkGroundSouth,sprites.vehicle.roadHorizontal,sprites.dungeon.darkGroundEast,sprites.dungeon.darkGroundSouthEast0], TileScale.Sixteen);
+            case "level2":
+            case "level2":return tiles.createTilemap(hex`0d0009000000000000000000000000000000050505030505050305050500000515050405130504050505000005150505051405050505010000060808080808190808080200000d07070f0e10160707070900000d0717110e12180707070900000c0b0b0b0b0b0b0b0b0b0a0000000000000000000000000000`, img`
+2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 . . . . . 2 . . . . . 2 
+2 . . . . . . . . . . . 2 
+2 . . . . . . 2 . . . . 2 
+2 . . . . . . . . . . . 2 
+2 . . . . . . . . . . . 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 
+`, [myTiles.transparency16,myTiles.tile1,myTiles.tile2,myTiles.tile4,myTiles.tile5,myTiles.tile6,sprites.dungeon.darkGroundNorthWest0,sprites.dungeon.darkGroundCenter,sprites.dungeon.darkGroundNorth,sprites.dungeon.darkGroundEast,sprites.dungeon.darkGroundSouthEast0,sprites.dungeon.darkGroundSouth,sprites.dungeon.darkGroundSouthWest0,sprites.dungeon.darkGroundWest,sprites.vehicle.roadHorizontal,myTiles.tile7,myTiles.tile8,myTiles.tile9,myTiles.tile10,myTiles.tile11,myTiles.tile12,myTiles.tile13,myTiles.tile14,myTiles.tile3,myTiles.tile15,myTiles.tile17], TileScale.Sixteen);
+        }
+        return null;
+    })
+
+    helpers._registerFactory("tile", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "transparency16":return transparency16;
+            case "myTile":
+            case "tile1":return tile1;
+            case "myTile0":
+            case "tile2":return tile2;
+            case "myTile2":
+            case "tile4":return tile4;
+            case "myTile3":
+            case "tile5":return tile5;
+            case "myTile4":
+            case "tile6":return tile6;
+            case "myTile5":
+            case "tile7":return tile7;
+            case "myTile6":
+            case "tile8":return tile8;
+            case "myTile7":
+            case "tile9":return tile9;
+            case "myTile8":
+            case "tile10":return tile10;
+            case "myTile9":
+            case "tile11":return tile11;
+            case "myTile10":
+            case "tile12":return tile12;
+            case "myTile11":
+            case "tile13":return tile13;
+            case "myTile12":
+            case "tile14":return tile14;
+            case "myTile1":
+            case "tile3":return tile3;
+            case "myTile13":
+            case "tile15":return tile15;
+            case "myTile15":
+            case "tile17":return tile17;
+            case "myTile14":
+            case "tile16":return tile16;
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
